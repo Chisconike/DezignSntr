d7323a2f149fe0f77a07250842c13afd8444bddd
Chisconike <chinedue856@gmail.com>
2023-10-04 18:19:05 +0000

    added allot

--- assest/js/app.js
+++ assest/js/app.js
@@ -7,6 +7,13 @@ $(document).ready(function () {
     let $content = $(this).closest('.each').find('.hide');
     $content.slideToggle(300);
     $(this).toggleClass('add remove').html($(this).hasClass('add') ? 'add' : 'remove');
+
+    let color = $(this).closest('.each').find('.topic');
+    if ($(this).hasClass('add')) {
+      color.css('color', '#6A6960');
+    } else {
+      color.css('color', '#E9FF9D');
+    }
   })
 })
 
